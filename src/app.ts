6da3252aebcb25193fc3af3a691f65b233da6a8d
Prasad Medarametla <varaprasad.medarametla@omniwyse.com>
2020-11-13 12:57:40 +0530

    Stange but valid, CRM is expecting order for odata.type in JSON

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,26 @@
-import * as express from "express";
+import express from "express";
 import { NorthwindServer } from "./server";
 
-NorthwindServer.create(3000);
+
+const app = express();
+
+app.listen(3000);
+
+
+app.use((req, res, next) => {
+  // tslint:disable-next-line: no-string-literal
+  if(req.headers.accept && req.headers.accept.indexOf("charset") < 0){
+    req.headers.accept += ";charset=utf-16";
+  }
+  req.headers["accept-charset"] = undefined;
+  next()
+})
+
+app.set("json replacer", (key,value) => {
+  if(this && typeof(key) === "string" && key.indexOf("@odata.type") > -1){
+    value = undefined
+  }
+  return value
+});
+
+app.use("/", NorthwindServer.create());
